6bba02f15523804ecb25cb9ebec43f24985d945a
mojo <igormalj@gmail.com>
2024-04-23 18:11:59 +0200

    add tailwind css with config

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const app: Express = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req: Request, res: Response) => {
   res.render('index');
