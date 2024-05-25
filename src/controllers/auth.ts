acbe9d1ca4f072d9a18cc7bb99714c4fa00c1b1d
mojo <igormalj@gmail.com>
2024-05-25 20:38:02 +0200

    add basic user creation

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import asyncHandler from 'express-async-handler';
 import { validationResult, FieldValidationError } from 'express-validator';
-import User from 'src/models/user';
+import User from '../models/user';
+import { generatePassword } from '../utils/passwordHelpers';
 
 const registerGet = (req: Request, res: Response) => {
   res.status(200).render('pages/register', {
@@ -9,27 +10,37 @@ const registerGet = (req: Request, res: Response) => {
   });
 };
 
-const registerPost = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    const { password, passwordRepeat } = req.body;
+const registerPost = asyncHandler(async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  const { firstName, lastName, username, password, passwordRepeat } = req.body;
 
-    const equalPasswords = password === passwordRepeat;
+  const equalPasswords = password === passwordRepeat;
 
-    if (!errors.isEmpty() && !equalPasswords) {
-      const errorsObj: { [key: string]: string } = {};
-      errors.array().forEach(err => {
-        const { path, msg } = err as FieldValidationError;
-        errorsObj[path] = msg;
-      });
-      if (!equalPasswords)
-        errorsObj.notEqualPasswords = 'passwords must be equal';
+  if (!errors.isEmpty() && !equalPasswords) {
+    const errorsObj: { [key: string]: string } = {};
+    errors.array().forEach(err => {
+      const { path, msg } = err as FieldValidationError;
+      errorsObj[path] = msg;
+    });
 
-      return res.status(200).render('pages/sign-up', { errors: errorsObj });
-    }
-    res.status(200).render('pages/sign-up', { errors: {} });
-  },
-);
+    if (!equalPasswords)
+      errorsObj.notEqualPasswords = 'passwords must be equal';
+
+    return res.status(200).render('pages/register', { errors: errorsObj });
+  }
+
+  const hashedPassword = await generatePassword(password);
+  const newUser = new User({
+    firstName,
+    lastName,
+    username,
+    password: hashedPassword,
+  });
+
+  await newUser.save();
+
+  return res.status(200).redirect('/login');
+});
 
 const loginGet = (req: Request, res: Response) => {
   res.status(200).render('pages/login', {
